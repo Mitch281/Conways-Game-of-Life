df73142b113be6bc26bf2f41c647df59c24403ad
Mitchell Anton <antonm@student.unimelb.edu.au>
2021-08-05 23:56:17 +1000

    Added cell highlighting when cell is pressed (these are the alive cells).

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import numpy as np
 
 pygame.init()
 
@@ -10,7 +11,22 @@ BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 GREEN = (0, 255, 0)
 
+grid = np.zeros((NUM_ROWS, NUM_COLUMNS))
 
+# Handles logic and algorithm.
+class Puzzle:
+    def __init__(self):
+        self.grid = grid
+
+    # Changes the grid number to 1 if a square is clicked.
+    def fill_grid(self, click_position):
+        x_pos = click_position[0]
+        y_pos = click_position[1]
+        col_num = x_pos // INCREMENT
+        row_num = y_pos // INCREMENT
+        self.grid[row_num][col_num] = 1
+
+# Handles GUI
 class Screen:
     def __init__(self):
         self.display = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_LENGTH))
@@ -21,8 +37,18 @@ class Screen:
             pygame.draw.line(self.display, WHITE, (i, 0), (i, SCREEN_LENGTH))
             pygame.draw.line(self.display, WHITE, (0, i), (SCREEN_WIDTH, i))
 
+    # Renders a green box on top of the cells that are alive.
+    def render_alive_cells(self):
+        for row_num in range(NUM_ROWS):
+            if 1 in puzzle.grid[row_num]:
+                for col_num in range(NUM_COLUMNS):
+                    if puzzle.grid[row_num][col_num] == 1:
+                        y_pos = row_num * INCREMENT
+                        x_pos = col_num * INCREMENT
+                        pygame.draw.rect(self.display, GREEN, (x_pos, y_pos, INCREMENT, INCREMENT))
 
 screen = Screen()
+puzzle = Puzzle()
 
 
 def main():
@@ -30,9 +56,13 @@ def main():
     while running:
         screen.display.fill(BLACK)
         screen.draw_lines()
+        screen.render_alive_cells()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                click_position = pygame.mouse.get_pos()
+                puzzle.fill_grid(click_position)
 
         pygame.display.update()
 
