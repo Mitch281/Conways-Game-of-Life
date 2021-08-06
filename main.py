c3e4d98f275917c26ec50301418b263dc542c9f9
Mitchell Anton <antonm@student.unimelb.edu.au>
2021-08-06 21:09:06 +1000

    Algorithm is done for case where cells are not on a boundary. Just need to get case where cell is on boundary.

--- main.py
+++ main.py
@@ -14,6 +14,8 @@ GREEN = (0, 255, 0)
 grid = np.zeros((NUM_ROWS, NUM_COLUMNS))
 
 # Handles logic and algorithm.
+
+
 class Puzzle:
     def __init__(self):
         self.grid = grid
@@ -26,63 +28,20 @@ class Puzzle:
         row_num = y_pos // INCREMENT
         self.grid[row_num][col_num] = 1
 
+    
+    # Returns true if a cell is not on a boundary, false otherwise.
+    def not_on_boundary(self, row_num, col_num):
+        if 1 <= row_num <= NUM_ROWS - 2 and 1 <= col_num <= NUM_COLUMNS - 2:
+            return True
+        return False
+
     # Gets the status of all neighbours of a specific cell.
+
     def get_num_alive_neighbours(self, row_num, col_num):
         neighbour_values = []
 
-        # Cell is on top boundary
-        if row_num == 0:
-            # Cell is in top left corner
-            if col_num == 0:
-                neighbour_values.append(self.grid[0][1])
-                neighbour_values.append(self.grid[1][1])
-                neighbour_values.append(self.grid[1][0])
-            # Cell is in top right corner
-            elif col_num == NUM_COLUMNS - 1:
-                neighbour_values.append(self.grid[0][NUM_COLUMNS - 2])
-                neighbour_values.append(self.grid[1][NUM_COLUMNS - 2])
-                neighbour_values.append(self.grid[1][NUM_COLUMNS - 1])
-            else:
-                for i in range(-1, 2):
-                    neighbour_values.append(self.grid[1][col_num + i])
-                neighbour_values.append(self.grid[0][col_num - 1])
-                neighbour_values.append(self.grid[0][col_num + 1])
-
-        # Cell is on right boundary
-        if col_num == NUM_COLUMNS - 1:
-            # Cell is in bottom right corner
-            if row_num == NUM_ROWS - 1:
-                neighbour_values.append(self.grid[NUM_ROWS - 2][NUM_COLUMNS - 1])
-                neighbour_values.append(self.grid[NUM_ROWS - 2][NUM_COLUMNS - 2])
-                neighbour_values.append(self.grid[NUM_ROWS - 1][NUM_COLUMNS - 2])
-            else:
-                for i in range(-1, 2):
-                    neighbour_values.append(self.grid[row_num + i][col_num - 1])
-                neighbour_values.append(self.grid[row_num - 1][col_num])
-                neighbour_values.append(self.grid[row_num + 1][col_num])
-
-        # Cell is on bottom boundary.
-        if row_num == NUM_ROWS - 1:
-            # Cell is in bottom left corner
-            if col_num == 0:
-                neighbour_values.append(self.grid[NUM_ROWS - 2][0])
-                neighbour_values.append(self.grid[NUM_ROWS - 2][1])
-                neighbour_values.append(self.grid[NUM_ROWS - 1][1])
-            else:
-                for i in range(-1, 2):
-                    neighbour_values.append(self.grid[NUM_ROWS - 2][col_num + i])
-                neighbour_values.append(self.grid[row_num][col_num - 1])
-                neighbour_values.append(self.grid[row_num][col_num + 1])
-
-        # Cell is on left boundary
-        if col_num == 0:
-            for i in range(-1, 2):
-                neighbour_values.append(self.grid[row_num + i][col_num + 1])
-            neighbour_values.append(self.grid[row_num - 1][0])
-            neighbour_values.append(self.grid[row_num + 1][0])
-
-        # Cell is not on any boundary
-        else:
+        # Ensures that the cell is not on a boundary.
+        if self.not_on_boundary(row_num, col_num):
             for i in range(-1, 2):
                 neighbour_values.append(grid[row_num - 1][col_num + i])
                 neighbour_values.append(grid[row_num + 1][col_num + i])
@@ -91,26 +50,33 @@ class Puzzle:
 
         return neighbour_values.count(1)
 
-
-
-
     # Returns an array of all the indexes of the cells that are to become alive and are to die.
+
     def get_births_and_deaths(self):
         births = []
         deaths = []
         for row_num in range(NUM_ROWS):
             for col_num in range(NUM_COLUMNS):
+
+                # The cell is currently alive.
+                if self.grid[row_num][col_num] == 1:
+                    if self.not_on_boundary(row_num, col_num):
+                        # We kill the cell due to under or over population.
+                        if self.get_num_alive_neighbours(row_num, col_num) <= 1 or \
+                            self.get_num_alive_neighbours(row_num, col_num) >= 4:
+                            deaths.append((row_num, col_num))
+
+                # The cell is currently not alive.
                 if self.grid[row_num][col_num] == 0:
-                    if self.get_num_alive_neighbours(row_num, col_num) == 3:
-                        births.append((row_num, col_num))
-                elif self.grid[row_num][col_num] == 1:
-                    if (0 <= self.get_num_alive_neighbours(row_num, col_num) <= 1) or \
-                    (self.get_num_alive_neighbours(row_num, col_num) >= 4):
-                        deaths.append((row_num, col_num))
+                    if self.not_on_boundary(row_num, col_num):
+                        # A new cell is born.
+                        if self.get_num_alive_neighbours(row_num, col_num) == 3:
+                            births.append((row_num, col_num))
+
         return births, deaths
-
-
+                    
     # The algorithm that runs the game.
+
     def run_game(self):
         births = self.get_births_and_deaths()[0]
         deaths = self.get_births_and_deaths()[1]
@@ -128,6 +94,8 @@ class Puzzle:
             self.grid[row_num][col_num] = 0
 
 # Handles GUI
+
+
 class Screen:
     def __init__(self):
         self.display = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_LENGTH))
@@ -146,7 +114,9 @@ class Screen:
                     if puzzle.grid[row_num][col_num] == 1:
                         y_pos = row_num * INCREMENT
                         x_pos = col_num * INCREMENT
-                        pygame.draw.rect(self.display, GREEN, (x_pos, y_pos, INCREMENT, INCREMENT))
+                        pygame.draw.rect(self.display, GREEN,
+                                         (x_pos, y_pos, INCREMENT, INCREMENT))
+
 
 screen = Screen()
 puzzle = Puzzle()
@@ -169,5 +139,6 @@ def main():
                     puzzle.run_game()
 
         pygame.display.update()
+
 
 main()
